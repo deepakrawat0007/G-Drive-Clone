8be3eb403b6dfe7723d740e084c8581867eb4fa2
deepakrawat0007 <deepakrawat3738@gmail.com>
2023-04-18 13:35:25 +0530

    few fixes

--- G-Drive-API/index.js
+++ G-Drive-API/index.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-
-const port = 5000;
+const dotenv = require("dotenv")
+dotenv.config()
+const port = process.env.DATABASE_PORT || 5000;
 const API = process.env.DATABASE_URL || "mongodb+srv://root:root123@cluster0.hzhvoqr.mongodb.net/G-Drive?retryWrites=true&w=majority"
 
 mongoose.set('strictQuery', false);
